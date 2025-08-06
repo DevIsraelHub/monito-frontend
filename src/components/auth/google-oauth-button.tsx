3cbd20cf33d70c83bf84fdf6850f76bd5d690206
Dev Israel <devisrael01@gmail.com>
2025-08-06 12:57:20 +0100

    update

--- src/components/auth/google-oauth-button.tsx
+++ src/components/auth/google-oauth-button.tsx
@@ -1,24 +1,26 @@
-import { baseURL } from "@/lib/base-url";
-import { Button } from "../ui/button";
-import { FcGoogle } from "react-icons/fc";
+import { Info } from "lucide-react";
+import { Card, CardHeader } from "../ui/card";
 
-const GoogleOauthButton = (props: { label: string }) => {
-  const { label } = props;
+const GoogleOauthButton = () => {
 
-  const handleClick = () => {
-    window.location.href = `${baseURL}/auth/google`;
-  };
   return (
-    <Button
-      disabled
-      onClick={handleClick}
-      variant="outline"
-      type="button"
+    <Card
       className="w-full"
     >
-      <FcGoogle />
-      {label} with Google
-    </Button>
+      <CardHeader className="flex flex-col text-sm gap-2">
+        <span className="flex flex-row items-center gap-2"><Info size={16} /> Use this credentials for easy login :</span>
+        <div className="flex flex-row items-center gap-2">
+          <div className="text-sm text-muted-foreground flex flex-col gap-2">
+            <div className="flex flex-row items-center gap-2" >
+              Email:  <span className="font-bold">john@gmail.com</span>
+            </div>
+            <div className="flex flex-row items-center gap-2" >
+              Password <span className="font-bold">john</span>
+            </div>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
   );
 };
 
--- src/page/auth/Sign-in.tsx
+++ src/page/auth/Sign-in.tsx
@@ -70,7 +70,7 @@ const SignIn = () => {
       onError: (error) => {
         toast({
           title: "Error",
-          description: error.message,
+          description: (error as any).response.data.message,
           variant: "destructive",
         });
       },
@@ -94,7 +94,7 @@ const SignIn = () => {
                 <CardHeader className="text-center">
                   <CardTitle className="text-xl">Welcome back</CardTitle>
                   <CardDescription>
-                    Login with your Email or Google account
+                    Login with your Email & Password
                   </CardDescription>
                 </CardHeader>
                 <div>
@@ -102,7 +102,7 @@ const SignIn = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)}>
                       <div className="grid gap-6">
                         <div className="flex flex-col gap-4">
-                          <GoogleOauthButton label="Login" />
+                          <GoogleOauthButton />
                         </div>
                         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                           <span className="relative z-10 bg-background px-2 text-muted-foreground">
